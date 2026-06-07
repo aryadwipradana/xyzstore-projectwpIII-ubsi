90dba5f85dc74ad44e8412841a7b81350c0de45d
aryadwip <dagung34@gmail.com>
2026-06-07 20:08:56 +0700

    update login

--- resources/views/backend/v_login/login.blade.php
+++ resources/views/backend/v_login/login.blade.php
@@ -96,8 +96,7 @@
        <div class="col-12">
         <div class="form-group">
          <div class="p-t-20">
-          <button class="btn btn-info" id="to-recover" type="button"><i class="fa fa-lock m-r-5"></i> Lost
-           password?</button>
+         
           <button type="submit" class="btn btn-success float-right" type="submit">Login</button>
          </div>
         </div>
